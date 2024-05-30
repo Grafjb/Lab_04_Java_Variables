dd7842a8c70471cc845480ddc9911c470292205e
Jake <grafjb@mail.uc.edu>
2024-05-30 15:00:00 -0400

    Commit 1

---
+++ src/Main.java
@@ -0,0 +1,56 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        int intOperandA = 1;
+        int intOperandB = 5;
+        int intSum ;
+        int intProduct = 24;
+        int intDifference = 8;
+        int intQuotient = 36;
+        int intModulo = 17;
+
+        double doubleOperandA = 3.50;
+        double doubleOperandB = 2.75;
+        double doubleSum = 10.25;
+        double doubleProduct = 15.75;
+        double doubleDifference = 5.50;
+        double doubleQuotient = 7.20;
+
+        intSum = intOperandA + intOperandB;
+        intProduct = intOperandA * intOperandB;
+        intDifference = intOperandA - intOperandB;
+        intQuotient = intOperandA / intOperandB;
+        intModulo = intOperandA % intOperandB;
+
+        System.out.println("The sum using ints of " + intOperandA + " + " + intOperandB + " is " + intSum);
+        System.out.println("The product using ints of " + intOperandA + " * " + intOperandB + " is " + intProduct);
+        System.out.println("The difference using ints of " + intOperandA + " - " + intOperandB + " is " + intDifference);
+        System.out.println("The quotient using ints of " + intOperandA + " / " + intOperandB + " is " + intQuotient);
+        System.out.println("The modulo using ints of " + intOperandA + " % " + intOperandB + " is " + intModulo);
+
+
+        doubleSum = doubleOperandA + doubleOperandB;
+        doubleProduct = doubleOperandA * doubleOperandB;
+        doubleDifference = doubleOperandA - doubleOperandB;
+        doubleQuotient = doubleOperandA / doubleOperandB;
+
+        System.out.println("The sum using doubles of " + doubleOperandA + " + " + doubleOperandB + " is " + doubleSum);
+        System.out.println("The product using doubles of " + doubleOperandA + " * " + doubleOperandB + " is " + doubleProduct);
+        System.out.println("The difference using doubles of " + doubleOperandA + " - " + doubleOperandB + " is " + doubleDifference);
+        System.out.println("The quotient using doubles of " + doubleOperandA + " / " + doubleOperandB + " is " + doubleQuotient);
+
+        double myLunchCost = 12.50;
+        int numberOfKidsInFamily = 3;
+        boolean isRaining = false;
+        double priceOfGallonOfGas = 3.25;
+        int favoriteNumber = 7;
+        double shoeSize = 9.5;
+        String birthMonth = "October";
+        String fullName = "John Doe";
+
+
+    }
+
+}
